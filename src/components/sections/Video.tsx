81c358b2c89fbaff9e849b3e1c2496d61b85f9aa
Vibing Dev <vibingdev5@gmail.com>
2025-07-09 06:54:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -22,7 +22,7 @@ export default function Video() {
           */}
           <div className="aspect-video">
              <iframe
-              src="https://www.youtube.com/embed/VIDEO_ID_HERE"
+              src="https://youtu.be/ouXulz0Isek?si=CHBPPfgghqy4pYyz"
               title="RugbyCare UG Impact Story"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
